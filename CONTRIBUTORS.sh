d580bcda577b140b81e2e624296343006f292da6
iyenagbe <iyenagberaymond@gmail.com>
2023-11-10 14:49:57 -0800

    0x16. C - Simple Shell

---
+++ CONTRIBUTORS.sh
@@ -0,0 +1,10 @@
+ 	
+
+	IYENAGBE RAYMOND OGEYANOR github.com/iyenagbe
+
+        OKON PATIENCE IMA-OBONG: github.com/pattyima
+
+                Fork the Repository.
+                Clone it.
+                Push it to your repository.
+                Pull request(Merge it)
