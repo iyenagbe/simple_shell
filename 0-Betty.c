8bd526259bbfaa2dc0d11f58d7c2ca5c9f86a7fa
pattyima <pattynaries@gmail.com>
2023-11-15 01:28:54 -0800

    Betty

--- 0-Betty.c
+++ 0-Betty.c
@@ -7,9 +7,7 @@
  */
 int main(void)
 {
-    for(int i = 1; i <= 10; ++i)
-        
-        printf("%d\n", i);
-
-        return (0);
+	for (int i = 1; i <= 10; ++i)
+		printf("%d\n", i);
+	return (0);
 }
